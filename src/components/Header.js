e6d1bec2fe23fd785467454c53ba5bf0dc7d43ae
Aditya Mukila <66318348+ZephyrIO@users.noreply.github.com>
2024-06-21 20:41:05 -0400

    created page header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+export default function Header ()
+{
+    return (
+        <div className="title">
+            <h1>Portfolio</h1>
+            <h4>Aditya Mukila</h4>
+        </div>
+    );
+}
