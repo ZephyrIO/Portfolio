8b4a54d458715cbcdc9f64fd0e27d66bb87686cd
Aditya Mukila <66318348+ZephyrIO@users.noreply.github.com>
2024-07-02 20:42:03 -0400

    feat: :sparkles: finished project list
    Added Dodge and Squash to the Project List

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import hangman from '../../public/assets/Hangman.png';
 import cnb from '../../public/assets/CowsandBulls.png';
 import ttt from '../../public/assets/TicTacToe.png';
+import dodge from '../../public/assets/DodgetheCreeps.webp';
+import squash from '../../public/assets/SquashtheCreeps.webp';
 
 import './ProjectList.css';
 
@@ -11,23 +13,37 @@ export default function ProjectList ()
         <div className="grid">
             <div>
                 <a href="https://github.com/ZephyrIO/Hangman-Java">
-                    <Image src={hangman} alt="Hangman Game in the Terminal" width={500} height={400} />
+                    <Image src={hangman} alt="Hangman Game in the Terminal" width={500} height={375} />
                     <br />
                     Hangman
                 </a>
             </div>
             <div>
                 <a href="https://github.com/ZephyrIO/CowsAndBulls-Java">
-                    <Image src={cnb} alt="Cows and Bulls (Wordle with Numbers) in the Terminal" width={500} height={400} />
+                    <Image src={cnb} alt="Cows and Bulls (Wordle with Numbers) in the Terminal" width={500} height={375} />
                     <br />
                     Cows and Bulls
                 </a>
             </div>
             <div>
                 <a href="https://github.com/ZephyrIO/TicTacToe-Java">
-                    <Image src={ttt} alt="Tic-Tac-Toe in the Terminal" width={500} height={400} />
+                    <Image src={ttt} alt="Tic-Tac-Toe in the Terminal" width={500} height={375} />
                     <br />
                     Tic-Tac-Toe
+                </a>
+            </div>
+            <div>
+                <a href="https://github.com/ZephyrIO/Dodge-the-Creeps">
+                    <Image src={dodge} alt="Dodge the Creeps Demo Game from Godot Docs" />
+                    <br />
+                    Dodge the Creeps
+                </a>
+            </div>
+            <div>
+                <a href="https://github.com/ZephyrIO/Squash-the-Creeps">
+                    <Image src={squash} alt="Squash the Creeps Demo Game from Godot Docs" />
+                    <br />
+                    Squash the Creeps
                 </a>
             </div>
         </div>
