9e8209167b6c1649312615adbd06b9326099ba55
Aditya Mukila <66318348+ZephyrIO@users.noreply.github.com>
2024-07-03 21:46:11 -0400

    Reformatted the display of home

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,13 +8,18 @@ export default function Info ()
     return (
         <div className="text">
             <h2>Who am I?</h2>
-            <p className={karla.className}>
-                I am a Computer Science student at the University of Georgia and I am aiming to excel within the software development industry.
-                I am proficient with a multitude of programming languages such as Java, SQL, C/C++, C# and HTML. I am experienced in usage of
-                various game development engines such as Unity and Godot. I possess effective communication skills, and am able to clearly and
-                confidently explain myself and my ideas to others. I can work well in both a team or individually and can be efficient regardless
-                of my workflow.
-            </p>
+            <br />
+            <ul className={karla.className}>
+                <li>I am a Computer Science student at the University of Georgia and I am aiming to excel within the software development industry.</li>
+                <br />
+                <li>I am proficient with a multitude of programming languages such as Java, SQL, C/C++, C# and HTML.</li>
+                <br />
+                <li>I am experienced in usage of various game development engines such as Unity and Godot.</li>
+                <br />
+                <li>I possess effective communication skills, and am able to clearly and confidently explain myself and my ideas to others.</li>
+                <br />
+                <li>I can work well in both a team or individually and can be efficient regardless of my workflow.</li>
+            </ul>
         </div>
     )
 }
